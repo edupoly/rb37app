868a44de24f0623bb083da5aea6b400c771bb6b7
edupoly <info.edupoly@gmail.com>
2024-07-29 07:24:17 +0530

    xcvxcv

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function App() {
+  var [countries,setCountries]=useState([]);
+  axios.get("https://restcountries.com/v3/all").then(function(res){
+    setCountries([...res.data])
+  })
   return (
     <div className="App">
       <h1>India</h1>
+      <ul>
+        {
+          countries?.map((c)=>{
+            return <li>{c.name.common}</li>
+          })
+        }
+      </ul>
     </div>
   );
 }
